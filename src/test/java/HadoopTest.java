ad3dbb8bbcf775ba83bd722bbeed7f734fc0e262
codeworm111 <1169872430@qq.com>
2021-10-08 22:53:11 +0800

    HadoopTest API run well on ubantu

--- src/test/java/HadoopTest.java
+++ src/test/java/HadoopTest.java
@@ -11,14 +11,17 @@ import java.net.URISyntaxException;
 public class HadoopTest {
     public static void main(String[] args) throws IOException, InterruptedException {
 
-        //获取文件系统
+        //1 获取文件系统
         Configuration conf=new Configuration();
+        conf.addResource(new Path("/home/jiangxuzhao/Environment/hadoop-2.10.1/etc/hadoop/core-site.xml"));
+        conf.addResource(new Path("/home/jiangxuzhao/Environment/hadoop-2.10.1/etc/hadoop/hdfs-site.xml"));
+
         FileSystem fs=FileSystem.get(conf);
         // 2 创建输入流；路径前不需要加file:///，否则报错
         FileInputStream fis = new FileInputStream(new File("/home/jiangxuzhao/Desktop/Data/uploadtest"));
         // 3 创建输出流
         FSDataOutputStream fos = fs.create(new Path("hdfs://master:9000/uploadtest"),
-                true,4096,(short)1,496);
+                false,4096,(short)1,496);
         // 4 流对拷 org.apache.commons.io.IOUtils
         IOUtils.copy(fis,fos);
         // 5 关闭资源
